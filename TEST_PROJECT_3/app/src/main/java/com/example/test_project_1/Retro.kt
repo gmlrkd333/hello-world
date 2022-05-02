d6ce3ebf9cc27a98906aa087850f6ba22447f958
heegang <gmlrkd3160@naver.com>
2022-05-03 02:02:16 +0900

    xcvx

--- TEST_PROJECT_3/app/src/main/java/com/example/test_project_1/Retro.kt
+++ TEST_PROJECT_3/app/src/main/java/com/example/test_project_1/Retro.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class Retro{
     var retrofit = Retrofit.Builder()
-        .baseUrl("http://192.168.35.186:8000")
+        .baseUrl("http://:8000")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 }
